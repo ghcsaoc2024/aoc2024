3a1cb75f4a544665970120f11bd2e43a38d704c5
ghcsaoc2024 <adventofcode2024@preminger.xyz>
2024-12-08 09:40:34 -0500

    day 8

---
+++ day-08/puzzle-a/go.mod
@@ -0,0 +1,5 @@
+module dayeight
+
+go 1.23.4
+
+require github.com/hashicorp/go-set/v3 v3.0.0
---
+++ day-08/puzzle-a/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+
+	"dayeight/lib"
+
+	"github.com/hashicorp/go-set/v3"
+)
+
+func main() {
+	file, err := os.Open("../input/input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(file *os.File) {
+		closeErr := file.Close()
+		if closeErr != nil {
+			log.Fatal(closeErr)
+		}
+	}(file)
+
+	scanner := bufio.NewScanner(file)
+
+	// Read in the array
+	dimensions, antennae := lib.ReadArray(scanner)
+
+	// Find the antinodes
+	antinodes := set.New[lib.Coord](0)
+	for _, locs := range antennae {
+		for _, loc1 := range locs {
+			for _, loc2 := range locs {
+				if loc2 == loc1 {
+					continue
+				}
+				diff := loc2.Subtract(loc1)
+				projection := loc2.Add(diff)
+				if projection.IsValid(dimensions) {
+					antinodes.Insert(projection)
+				}
+			}
+		}
+	}
+
+	log.Printf("number of antinodes found: %d", antinodes.Size())
+}
