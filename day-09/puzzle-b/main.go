9a1c0416ffef0d835193962c6de378f682ede033
ghcsaoc2024 <adventofcode2024@preminger.xyz>
2024-12-09 10:53:04 -0500

    added some explanatory comments

--- day-09/puzzle-b/main.go
+++ day-09/puzzle-b/main.go
@@ -50,29 +50,38 @@ func main() {
 		fileStart++
 		fileEnd := lastFilledPtr + 1
 		fileLength := fileEnd - fileStart
+
 		if freeSpacePtr >= lastFilledPtr {
+			// The currently-pointed-to file does not fit in any free space to its left;
+			// reset the free space pointer to the leftmost free space, and...
 			freeSpacePtr = lo.IndexOf(diskContents, lib.FreeSpaceIndicator)
-			if freeSpacePtr == -1 {
-				break
-			}
+			// ...advance the last-filled pointer to the last filled space prior to the start
+			// of the currently-pointed-to file.
 			_, lastFilledPtr, _ = lo.FindLastIndexOf(diskContents[:fileStart], occupiedPred)
 			continue
 		}
 
+		// Calculate how much free space we're looking at
 		_, nextFilled, _ := lo.FindIndexOf(diskContents[freeSpacePtr:], occupiedPred)
 		if nextFilled == -1 {
 			nextFilled = len(diskContents)
 		} else {
 			nextFilled += freeSpacePtr
 		}
-
 		freeSpaceLength := nextFilled - freeSpacePtr
 
+		// Does the currently-pointed-to file fit in the free space?
 		if freeSpaceLength >= fileLength {
+			// It does! Move it over, overwriting its original position with empties.
 			emptyRun := lib.RunOf(fileLength, lib.FreeSpaceIndicator)
 			copy(diskContents[freeSpacePtr:], diskContents[fileStart:fileEnd])
 			copy(diskContents[fileStart:], emptyRun)
+			// Reset the free space pointer to the leftmost free space, and...
 			freeSpacePtr = lo.IndexOf(diskContents, lib.FreeSpaceIndicator)
+			// ...advance the last-filled pointer to the last filled space prior to the start
+			// of the currently-pointed-to file. (Remember: we don't get to try a single file
+			// more than once; so whatever lastFilledPtr has moved across, shall never be
+			// revisited again!)
 			_, lastFilledPtr, _ = lo.FindLastIndexOf(diskContents[:fileStart], occupiedPred)
 		} else {
 			freeSpacePtr = nextFilled + lo.IndexOf(diskContents[nextFilled:], lib.FreeSpaceIndicator)
