3093c50b8ae3de380294ec058ff26dd86a9caf15
ghcsaoc2024 <adventofcode2024@preminger.xyz>
2024-12-17 09:53:22 -0500

    brute-force version of part 2

---
+++ day-17/puzzle-b/main.go
@@ -0,0 +1,142 @@
+//nolint:mnd // Magic numbers all over the place
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"main/lib"
+	"math"
+	"os"
+	"reflect"
+
+	"github.com/alexflint/go-arg"
+)
+
+type Args struct {
+	InputFile  string `arg:"positional,required"        help:"input file"`
+	MaxTestVal int    `arg:"-m,--max-test-val,required" help:"maximum test value"`
+}
+
+func main() {
+	var args Args
+	arg.MustParse(&args)
+
+	if args.MaxTestVal < 1 {
+		log.Fatalf("max test val must be at least 1; got %d", args.MaxTestVal)
+	}
+
+	computer, program, err := readInputFile(args)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	log.Printf("computer: %v", *computer)
+	log.Printf("program: %v", *program)
+
+	for regA := range args.MaxTestVal {
+		isMatch, err := runProgram(*computer, *program)
+		if err != nil {
+			log.Panic(err)
+		}
+
+		if isMatch {
+			log.Printf("regA: %d", regA)
+			return
+		}
+	}
+
+	log.Printf("ended after %d iterations with no solution", args.MaxTestVal)
+}
+
+func runProgram(computer lib.Computer, program lib.Program) (bool, error) {
+	output := lib.Program{}
+	instructionPointer := 0
+	for instructionPointer < len(program) {
+		opCode := program[instructionPointer]
+		operand := program[instructionPointer+1]
+		switch opCode {
+		case lib.OpADiv, lib.OpBDiv, lib.OpCDiv:
+			numerator := computer.A
+			cOperand, err := comboOperand(computer, operand)
+			if err != nil {
+				return false, err
+			}
+			result := lib.Register(float64(numerator) / math.Pow(2, float64(cOperand)))
+
+			switch opCode {
+			case lib.OpADiv:
+				computer.A = result
+			case lib.OpBDiv:
+				computer.B = result
+			default:
+				computer.C = result
+			}
+		case lib.OpXOR:
+			computer.B = lib.Register(operand) ^ computer.B
+		case lib.OpStore:
+			cOperand, err := comboOperand(computer, operand)
+			if err != nil {
+				return false, err
+			}
+			computer.B = cOperand % 8
+		case lib.OpJumpNonZero:
+			if computer.A != 0 {
+				instructionPointer = int(operand)
+				continue
+			}
+		case lib.OpRegisterXOR:
+			computer.B ^= computer.C
+		case lib.OpOutput:
+			cOperand, err := comboOperand(computer, operand)
+			if err != nil {
+				return false, err
+			}
+			iOutput := len(output)
+			output = append(output, lib.OpVal(cOperand%8)) //nolint:gosec // Overflow intentional
+			if len(output) > len(program) || output[iOutput] != program[iOutput] {
+				return false, nil
+			}
+		}
+		instructionPointer += 2
+	}
+
+	if len(output) != len(program) {
+		return false, nil
+	}
+
+	return reflect.DeepEqual(output, program), nil
+}
+
+func comboOperand(computer lib.Computer, operand lib.OpVal) (lib.Register, error) {
+	switch operand {
+	case 0, 1, 2, 3:
+		return lib.Register(operand), nil
+	case 4:
+		return computer.A, nil
+	case 5:
+		return computer.B, nil
+	case 6:
+		return computer.C, nil
+	default:
+		return lib.Register(0), fmt.Errorf("invalid operand: %v", operand)
+	}
+}
+
+func readInputFile(args Args) (*lib.Computer, *lib.Program, error) {
+	file, err := os.Open(args.InputFile)
+	if err != nil {
+		log.Fatal(err) //nolint:revive // Toy code
+	}
+	defer func(file *os.File) {
+		closeErr := file.Close()
+		if closeErr != nil {
+			log.Fatal(closeErr) //nolint:revive // Toy code
+		}
+	}(file)
+
+	scanner := bufio.NewScanner(file)
+	computer, program, err := lib.ReadInput(scanner)
+
+	return computer, program, err //nolint:wrapcheck // Toy code
+}
